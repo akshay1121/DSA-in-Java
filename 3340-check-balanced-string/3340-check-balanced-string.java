b1f667cdade5ca0d9153d040eece729408cfb748
akshay1121 <akshay237632@gmail.com>
2026-07-07 16:35:16 +0530

    Time: 1 ms (98.62%), Space: 43.5 MB (48.39%) - LeetHub

---
+++ 3340-check-balanced-string/3340-check-balanced-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isBalanced(String num) {
+        int evensum=0;
+        int oddsum=0;
+        for(int i=0;i<num.length();i++){
+            int digit= num.charAt(i)-'0';
+            if(i%2==0){
+                evensum+=digit;
+            }else{
+                oddsum+=digit;
+            }
+        }
+        if(evensum==oddsum){
+            return true;
+        }
+        return false;
+    }
+}
